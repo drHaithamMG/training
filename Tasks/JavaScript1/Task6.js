54fc62e771d78dcb6ed5f3a2c7db04736fd6ebab
drhaithammg <dr.haithammg@gmail.com>
2020-12-09 13:22:48 +0200

    new

---
+++ Tasks/JavaScript1/Task6.js
@@ -0,0 +1,2 @@
+/*Question url:  https://jsfiddle.net/mk8pwfg1/ */
+/*My answer url: https://jsfiddle.net/haitham_mughrabi/f9uy4506/1/ */
---
+++ Tasks/JavaScript1/Task8.js
@@ -0,0 +1,2 @@
+/*Question url:  https://jsfiddle.net/4160amno/ */
+/*My answer url: https://jsfiddle.net/haitham_mughrabi/xcse2af0/ */

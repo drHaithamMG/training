8a7f0587a19f92c70102a4bfe808c5b4dc1aa40e
drhaithammg <dr.haithammg@gmail.com>
2020-12-09 16:48:43 +0200

    15

--- Tasks/JavaScript1/Task6.js
+++ Tasks/JavaScript1/Task6.js
@@ -1,2 +1,16 @@
 /*Question url:  https://jsfiddle.net/mk8pwfg1/ */
-/*My answer url: https://jsfiddle.net/haitham_mughrabi/f9uy4506/1/ */
+/*My answer url: https://jsfiddle.net/haitham_mughrabi/f9uy4506/2/*/
+function makeArrayOfArrays(x){
+    const mother=[];
+    for(let row =1;row<=x;row++){
+        let daughter=[];
+        for(let col=1;col<=x;col++){
+            daughter.push(col*row);
+        }
+        mother.push(daughter);
+        daughter=[];
+    }
+    return mother;
+}
+const x=prompt("Enter a value :");
+console.log(makeArrayOfArrays(x));

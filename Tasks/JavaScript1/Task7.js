d5c5300f48c24aa3b978085f0e42d489d2fb99bf
drhaithammg <dr.haithammg@gmail.com>
2020-12-09 15:04:29 +0200

    7

--- Tasks/JavaScript1/Task7.js
+++ Tasks/JavaScript1/Task7.js
@@ -1,2 +1,20 @@
 /*Question url:  https://jsfiddle.net/m3wughz6/ */
-/*My answer url: https://jsfiddle.net/haitham_mughrabi/c0svdLhw/ */
+/*My answer url: https://jsfiddle.net/haitham_mughrabi/c0svdLhw/4/ */
+
+function sortWord(string) {
+    let arr = string.split(' ');
+    arr.forEach((word, wordIndex) => {
+        let temp;
+        for (latter of word) {
+            if (isFinite(latter)) {
+                temp = Number(latter);
+                arr.splice(temp, 0, word);
+                arr.splice(wordIndex, 1);
+                break;
+            }
+        }
+    });
+    return arr;
+}
+let sentence = "is2 Thi1s T4est 3a";
+console.log(sortWord(sentence));
